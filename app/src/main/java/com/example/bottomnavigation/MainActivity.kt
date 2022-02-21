cad8d1e1d3880ab02e7ba6c32c8601ccb6eae739
ArturAndKotln <98820035+ArturAndKotln@users.noreply.github.com>
2022-02-21 21:18:09 +0300

    ok no 2 tab

--- app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -1,38 +1,31 @@
 package com.example.bottomnavigation
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import android.os.Bundle
 import com.example.bottomnavigation.databinding.ActivityMainBinding
-import com.example.material.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
 
 class MainActivity : AppCompatActivity() {
-
-    private var binding: ActivityMainBinding? = null
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-
-        supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
-
-        binding?.bottomNav?.setOnItemReselectedListener { item ->
-
-            when(item.itemId) {
-                R.id.homeItemBotNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
-                R.id.shopItemBotNav -> {}
-                R.id.carItemBotNav -> {}
-                R.id.accountItemBotNav -> {}
+        //установка первого фрогмено home
+//        supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
+        binding.bottomNav.setOnItemReselectedListener{item ->
+//
+            when(item.itemId){
+                R.id.homeItemBotNav -> supportFragmentManager.beginTransaction().replace(androidx.appcompat.R.id.content, Home()).commit()
+                R.id.shopItemBotNav -> supportFragmentManager.beginTransaction().replace(androidx.appcompat.R.id.content, Shop()).commit()
+                R.id.carItemBotNav -> supportFragmentManager.beginTransaction().replace(androidx.appcompat.R.id.content, Car()).commit()
+                R.id.accountItemBotNav -> supportFragmentManager.beginTransaction().replace(androidx.appcompat.R.id.content, Account()).commit()
             }
-
             return@setOnItemReselectedListener
-
-
         }
-        binding?.bottomNav?.selectedItemId = R.id.homeItemBotNav
+        binding.bottomNav.selectedItemId = R.id.homeItemBotNav
     }
 
 }
